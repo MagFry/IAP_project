7e4dd78de38738701a7b68f23efcb8274e05690a
Magda <mfryczka96@gmail.com>
2019-06-16 16:08:03 +0200

    updating endpoints

--- src/employees/urls.py
+++ src/employees/urls.py
@@ -3,8 +3,8 @@ from employees import views as employees_views
 
 
 urlpatterns = [
-    path('employees/', employees_views.employees_list),
-    path('employees/<int:pk>/', employees_views.employees_detail),
-    path('employee_hours/list/', employees_views.employees_hours),
-    path('salaries/', employees_views.employees_salaries),
+    path('api/employees/', employees_views.employees_list),
+    path('api/employees/<int:pk>/', employees_views.employees_detail),
+    path('api/employee_hours/list/', employees_views.employees_hours),
+    path('api/salaries/', employees_views.employees_salaries),
 ]
