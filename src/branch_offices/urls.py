3a1c49a9629c9f9651569dd8130319c3e1e61dfa
Ewa Czechowska <ewa@ai-traders.com>
2019-06-16 12:39:55 +0200

    cherry-pick: add api before each endpoint and make endpoint listing employees by branchOfficeId same as in spec

--- src/branch_offices/urls.py
+++ src/branch_offices/urls.py
@@ -5,4 +5,4 @@ from branch_offices import views as branch_offices_views
 urlpatterns = [
     path('api/branch_offices/', branch_offices_views.branch_offices_list),
     path('api/branch_offices/<int:pk>/', branch_offices_views.branch_offices_detail),
-]
+    ]
--- src/employees/urls.py
+++ src/employees/urls.py
@@ -3,7 +3,10 @@ from employees import views as employees_views
 
 
 urlpatterns = [
-    path('api/employees/', employees_views.employees_list),
+    # bad request, missing parameter: branchOfficeId
+    path('api/employees/list/', employees_views.invalid_employees_list),
+    # Gets a collection of employee objects by branchOfficeId
+    path('api/employees/list/<int:branchOfficeId>/', employees_views.employees_list),
     path('api/employees/<int:pk>/', employees_views.employees_detail),
     path('api/employee_hours/list/', employees_views.employees_hours),
     path('api/salaries/', employees_views.employees_salaries),
